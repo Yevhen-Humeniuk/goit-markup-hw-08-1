61ab2bf07b3f7bcb8d88a85d761e06b22dd64e45
Yevhen-Humeniuk <gumenyukevgen2@gmail.com>
2022-03-31 12:47:37 +0300

    start
    add imgs and start files

---
+++ js/header-mobile.js
@@ -0,0 +1,14 @@
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector("[header-mobile-open]"),
+      closeModalBtn: document.querySelector("[header-mobile-close]"),
+      modal: document.querySelector("[header-mobile]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
+    }
+  })();
